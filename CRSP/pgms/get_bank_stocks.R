ea05a285779725d01b990e6865cb1ab23b73129b
Blake Marsh <j1wbm01@wrds-sas6-w.wharton.private>
2021-05-30 11:53:51 -0400

    adding CCAR FBOs and fixing CCAR list

--- CRSP/pgms/get_bank_stocks.R
+++ CRSP/pgms/get_bank_stocks.R
@@ -1,7 +1,7 @@
 #---------------------------------------------------------------------
 # Downloads daily stock prices for permcos
 # listed in the NY Fed's PERMCO-RSSD link
-# Available at: 
+# Available at:
 # https://www.newyorkfed.org/research/banking_research/datasets.html
 #---------------------------------------------------------------------
 
@@ -35,9 +35,29 @@ nyfed = nyfed[!is.na(permco),]
 nyfed[,dt_start := as.Date(as.character(dt_start), "%Y%m%d")]
 nyfed[,dt_end := as.Date(as.character(dt_end), "%Y%m%d")]
 
+## flag for in NYFED data
+nyfed[,nyfed := 1]
+
 print(paste("Duplicate permcos?:", any(duplicated(nyfed[,list(permco, dt_start)]))))
 saveRDS(nyfed, "./data/nyfed_crsp_20200930.rds")
 write_feather(nyfed, "./data/nyfed_crsp_20200930.feather")
+
+#--------------
+# CCAR permcos
+#--------------
+CCAR_permcos = c(53687, 90, 3151, 20265, 30513,
+                 20483, 55006, 52396, 1741, 35048,
+                 2093, 20436, 2535, 1689, 21224,
+                 3275, 3685, 1620, 4260, 1645,
+                 21305, 4163, 21691, 20269,
+                 29146, 42125, 42291, 35175, 22107,
+                 29151, 29152, 55100, 20260)
+				 
+#-----------------------------
+# List of unique bank permcos
+#-----------------------------
+permcos = paste(unique(c(nyfed$permco, CCAR_permcos)), sep=' ', collapse=',')
+length(unique(c(nyfed$permco, CCAR_permcos)))
 
 #----------------------------------------
 # Get all stock prices for NYFED permcos
@@ -56,8 +76,8 @@ q = paste("SELECT a.*, b.htick, b.hcomnam, c.gvkey
                          AND linkprim in ('P', 'C')) as c
                    ON a.permno = c.lpermno
                   AND a.date between c.linkdt AND coalesce(c.linkenddt, CAST('9999-12-31' AS DATE))
-           WHERE a.date >= CAST('1986-06-30' AS DATE) 
-             AND a.permco IN (",  paste(nyfed$permco, sep=' ', collapse=','), ")", sep="")
+           WHERE a.date >= CAST('1986-01-01' AS DATE)
+             AND a.permco IN (",  permcos, ")", sep="")
 
 q = dbSendQuery(wrds, q)
 bank_stocks = dbFetch(q)
@@ -70,29 +90,37 @@ setDT(bank_stocks)
 any(duplicated(bank_stocks[,list(date, permco, permno)]))
 
 
-#-----------------------------
-# limit to NYFED active dates
-#-----------------------------
+#---------------------
+# Merge in NYFed data
+#---------------------
 
-bank_stocks = sqldf("SELECT a.*, b.entity as ID_RSSD, b.name as bank_name, b.inst_type
-                     FROM bank_stocks as a,
-                          nyfed as b
-                     WHERE a.permco = b.permco 
-                       AND a.date between b.dt_start and b.dt_end")
+bank_stocks = sqldf("SELECT a.*, b.entity as ID_RSSD, b.name as bank_name, b.inst_type, b.nyfed
+                     FROM bank_stocks as a
+                       LEFT JOIN nyfed as b
+                              ON a.permco = b.permco
+                             AND a.date between b.dt_start and b.dt_end")
 setDT(bank_stocks)
 
 #-----------------
 # Flag CCAR banks
 #-----------------
-CCAR_permcos = c(53687, 90, 3151, 20265, 30513,
-                 20483, 55006, 52396, 1741, 35048,
-                 2093, 20436, 2535, 1689, 21224, 
-                 3275, 3685, 1620, 4260, 1645, 
-                 21305, 4163, 21691, 57031, 20269,
-                 29146, 42125, 42291, 35175, 22107,
-                 29151, 29152, 55100, 20260)
-
 bank_stocks[,CCAR := ifelse(permco %in% CCAR_permcos, 1, 0)]
+bank_stocks[,CCAR_FBO := ifelse(permco %in% c(20260, 20269, 22107, 29146, 29151, 29152, 35175, 42125, 42291, 55100), 1, 0)]
+
+#-----------------------------------------
+# Drop if not in NYFED and not a CCAR FBO
+#-----------------------------------------
+bank_stocks = bank_stocks[which(!is.na(nyfed) | CCAR_FBO == 1),]
+
+#---------------------
+# Set quarterly date
+#--------------------
+bank_stocks[,dt_qtr := fifelse(as.Date(as.yearqtr(date)+0.25)-1 == date, as.Date(as.yearqtr(date)+0.25)-1, as.Date(as.yearqtr(date))-1)]
+
+#------------------
+# Set monthly date
+#------------------
+bank_stocks[,dt_mnth := as.Date(as.yearmon(date)+(1/12))-1]
 
 #------------------
 # Check duplicates
@@ -105,15 +133,18 @@ print(paste("Duplicates by permnos, date, inst_type, id_rssd:", any(duplicated(b
 print(paste("Number of unique permcos in NYFED data:", length(unique(nyfed$permco))))
 print(paste("Number of unique permcos from CRSP data:", length(unique(bank_stocks$permco))))
 
+#----------------------
+# list CCAR_banks
+# BNP ADR is excluded
 #---------------------
-# Set quarterly date
-#--------------------
-bank_stocks[,dt_qtr := fifelse(as.Date(as.yearqtr(date)+0.25)-1 == date, as.Date(as.yearqtr(date)+0.25)-1, as.Date(as.yearqtr(date))-1)]
 
-#------------------
-# Set monthly date
-#------------------
-bank_stocks[,dt_mnth := as.Date(as.yearmon(date)+(1/12))-1]
+## All CCAR banks
+CCAR_banks = unique(bank_stocks[which(dt_qtr == as.Date('2020-06-30') & CCAR == 1),list(hcomnam, permco, permno, htick)])
+CCAR_banks[order(hcomnam),]
+
+## CCAR FBOs
+CCAR_FBOs = unique(bank_stocks[which(dt_qtr == as.Date('2020-06-30') & CCAR_FBO == 1),list(hcomnam, permco, permno, htick)])
+CCAR_FBOs[order(hcomnam),]
 
 #-------------
 # Export data
